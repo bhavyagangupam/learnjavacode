63477e4d450f13b9a420f35909f3d77af95a620d
unknown <281998bhavya@gmail.com>
2022-05-27 09:27:21 +0000

    amazonapp activity

---
+++ day-4/amazonapp/src/main/java/amazonapp/com/Amazonapp2Application.java
@@ -0,0 +1,13 @@
+package amazonapp.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amazonapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amazonapp2Application.class, args);
+	}
+
+}
---
+++ day-4/amazonapp/src/test/java/amazonapp/com/Amazonapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package amazonapp.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amazonapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
